e3c8bc466666dc524ba7f186365919d86d3ad955
mrMekdad <mekdadr6@gmail.com>
2023-02-10 04:30:00 +0100

    biomarker-panel-evaluator: add repository metadata and packaging

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="reda-2023-biomarker-panel-evaluator",
+    version="0.1.0",
+    description="Evaluation utilities for biomarker candidate panels and score summaries.",
+    author="Red@",
+    packages=["biomarker_panel_evaluator"],
+    package_dir={"": "src"},
+)
